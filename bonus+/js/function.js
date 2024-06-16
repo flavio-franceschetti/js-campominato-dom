62dfb2a016a209850bd3d8af7696125c60fe6807
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-16 19:31:02 +0200

    feat[bonus]: add difficulty change to game

--- bonus+/js/function.js
+++ bonus+/js/function.js
@@ -1,7 +1,7 @@
 // creiamo la funzione generica per la creazione di elementi html
-function createElement(tag, classToAdd) {
+function createElement(tag, classToAdd, secondClass) {
   const square = document.createElement(tag);
-  square.classList.add(classToAdd);
+  square.classList.add(classToAdd, secondClass);
   return square;
 }
 
@@ -9,4 +9,49 @@ function createElement(tag, classToAdd) {
 function randomNum(min, max) {
   let randomNumGenerate = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumGenerate;
+}
+
+// creo una funzione per selezionare la difficoltà
+function difficultyFunctionSelect(tabCells, classToAdd, secondClass) {
+  for (let i = 1; i <= tabCells; i++) {
+    //creo un nuovo square
+    const newSquare = createElement("div", classToAdd, secondClass);
+    // creo una variabile per stampare in console il numero della cella che viene cliccata
+    const squareNum = "Il numero della cella è: " + i;
+
+    // variabile delle celle safe senza bombe
+    let safeCells = tabCells - 16;
+
+    // aggiungo un evento al click ad ogni nuovo box
+    newSquare.addEventListener("click", function () {
+      // condizione per rendere cliccabile ogni cella una sola volta
+      if (
+        newSquare.classList.contains("clicked") ||
+        newSquare.classList.contains("clicked-bomb")
+      ) {
+        return;
+      }
+
+      if (!bombarr.includes(i)) {
+        // aggiungo la classe per dare il colore ad ogni cella che viene cliccata
+        newSquare.classList.add("clicked");
+        score += 1;
+        scoreCard.innerHTML = `Punteggio: ${score}`;
+        console.log(score);
+      } else {
+        newSquare.classList.add("clicked-bomb");
+        alert(`Hai perso!!! Il tuo punteggio è ${score}`);
+      }
+
+      if (score === safeCells) {
+        alert(`HAI VINTOOOO!!! IL TUO PUNTEGGIO E': ${score}`);
+      }
+
+      // stampo in console il numero della cella che viene cliccata
+      console.log(squareNum);
+    });
+
+    // inserisco il nuovo square nel mio container html
+    gridContainer.append(newSquare);
+  }
 }
--- bonus+/js/myscript.js
+++ bonus+/js/myscript.js
@@ -5,50 +5,78 @@ const gridContainer = document.querySelector(".grid-container");
 let scoreCard = document.getElementById("score");
 let score = 0;
 
-// variabile delle celle safe senza bombe
-let safeCells = 100 - 16;
+//prendo la variabile delle option della difficoltà
+let difficultySelector = document.getElementById("select-difficulty");
+// creo un eventlistener sulla select dove con "change" ogni volta che l'utente seleziona la difficoltà viene generata la tabella in base a ciò che viene selezionato
+difficultySelector.addEventListener("change", function () {
+  // creo una variabile con il valore che viene selezionato nell'elemento select
+  const difficultyValue = difficultySelector.value;
+  // creo le variabili da cambiare in base alla difficoltà che poi verranno inserite come argomenti della funzione
+  let tabCells;
+  let classToAdd;
+
+  // creo le condizioni dove viene selezionata la difficoltà e cosa succede
+  if (difficultyValue === "easy") {
+    tabCells = 100;
+    classToAdd = "square100";
+  } else if (difficultyValue === "medium") {
+    tabCells = 81;
+    classToAdd = "square81";
+  } else if (difficultyValue === "hard") {
+    tabCells = 49;
+    classToAdd = "square49";
+  }
+  // richiamo la funzione per stampare la griglia in base alla difficoltà selezionata
+  difficultyFunctionSelect(tabCells, classToAdd, "square");
+});
 
 // aggiungiamo un ciclo fino a 100 per inserire gli elementi
-for (let i = 1; i <= 100; i++) {
-  //creo un nuovo square
-  const newSquare = createElement("div", "square");
-  // creo una variabile per stampare in console il numero della cella che viene cliccata
-  const squareNum = "Il numero della cella è: " + i;
+// *************************************************************
+// for (let i = 1; i <= 100; i++) {
+//   //creo un nuovo square
+//   const newSquare = createElement("div", "square");
+//   // creo una variabile per stampare in console il numero della cella che viene cliccata
+//   const squareNum = "Il numero della cella è: " + i;
 
-  // aggiungo un evento al click ad ogni nuovo box
-  newSquare.addEventListener("click", function () {
-    // condizione per rendere cliccabile ogni cella una sola volta
-    if (
-      newSquare.classList.contains("clicked") ||
-      newSquare.classList.contains("clicked-bomb")
-    ) {
-      return;
-    }
+//   // variabile delle celle safe senza bombe
+//   let safeCells = 100 - 16;
 
-    if (!bombarr.includes(i)) {
-      // aggiungo la classe per dare il colore ad ogni cella che viene cliccata
-      newSquare.classList.add("clicked");
-      score += 1;
-      scoreCard.innerHTML = `Punteggio: ${score}`;
-      console.log(score);
-    } else {
-      newSquare.classList.add("clicked-bomb");
-      alert(`Hai perso!!! Il tuo punteggio è ${score}`);
-    }
+//   // aggiungo un evento al click ad ogni nuovo box
+//   newSquare.addEventListener("click", function () {
+//     // condizione per rendere cliccabile ogni cella una sola volta
+//     if (
+//       newSquare.classList.contains("clicked") ||
+//       newSquare.classList.contains("clicked-bomb")
+//     ) {
+//       return;
+//     }
 
-    if (score === safeCells) {
-      alert(`HAI VINTOOOO!!! IL TUO PUNTEGGIO E': ${score}`);
-    }
+//     if (!bombarr.includes(i)) {
+//       // aggiungo la classe per dare il colore ad ogni cella che viene cliccata
+//       newSquare.classList.add("clicked");
+//       score += 1;
+//       scoreCard.innerHTML = `Punteggio: ${score}`;
+//       console.log(score);
+//     } else {
+//       newSquare.classList.add("clicked-bomb");
+//       alert(`Hai perso!!! Il tuo punteggio è ${score}`);
+//     }
 
-    // stampo in console il numero della cella che viene cliccata
-    console.log(squareNum);
-  });
+//     if (score === safeCells) {
+//       alert(`HAI VINTOOOO!!! IL TUO PUNTEGGIO E': ${score}`);
+//     }
 
-  // inserisco il nuovo square nel mio container html
-  gridContainer.append(newSquare);
-}
+//     // stampo in console il numero della cella che viene cliccata
+//     console.log(squareNum);
+//   });
+
+//   // inserisco il nuovo square nel mio container html
+//   gridContainer.append(newSquare);
+// }
+// *************************************************************
 
 // richiamiamo in una variabile il bottone per iniziare il gioco
+
 const playBtn = document.querySelector(".play-btn");
 // gli aggiungiamo un evento click
 playBtn.addEventListener("click", function () {
